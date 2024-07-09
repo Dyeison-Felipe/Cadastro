b1bde02ce972b329d0ece0fb8e9f54fc1063d5b7
dyeisonFK <dyeisonfc@gmail.com>
2024-07-09 18:42:46 -0300

    feat: criação de arquivos

--- backend/src/database/data-source.ts
+++ backend/src/database/data-source.ts
@@ -4,15 +4,17 @@ import {CreateUsersTable1719770634909} from './migrations/1719770634909-CreateUs
 import User from '../app/entities/User'
 import { CreateAddressTable1719861091974 } from "./migrations/1719861091974-CreateAddressTable"
 import Address from "../app/entities/Address"
+import { config } from "dotenv";
+config()
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "admin",
-    database: "register",
-    synchronize: true,
+    type: process.env.DB_TYPE as any,
+    host: process.env.DB_LOCALHOST,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    synchronize: true,  
     logging: false,
     entities: [User, Address],
     migrations: [CreateUsersTable1719770634909, CreateAddressTable1719861091974],
